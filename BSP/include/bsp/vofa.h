3b303331beeab2cb9a4fcd20820a43efa1f89a18
EICOMPUTER\EiEddie <eieddie4u@outlook.com>
2025-08-10 17:34:13 +0800

    feat: `vofa` 协议

---
+++ BSP/include/bsp/vofa.h
@@ -0,0 +1,30 @@
+#ifndef BSP_INCLUDE_BSP_VOFA_H_
+#define BSP_INCLUDE_BSP_VOFA_H_
+
+#include <stdint.h>
+#include <memory.h>
+
+#include <bsp/err.h>
+#include <bsp/uart.h>
+
+
+/**
+ * @brief 使用 vofa+ 的 `JustFloat` 协议发送数据.
+ */
+inline static void bsp_vofa_send_f32(bsp_uart_e uart, const float *data,
+                                     uint16_t len)
+{
+  const uint32_t CH_MAX = 128;
+  BSP_ASSERT(len <= CH_MAX);
+
+  uint8_t buf[(CH_MAX + 1) * sizeof(float)];
+  uint32_t bytes = len * sizeof(float);
+  memcpy(buf, data, bytes);
+
+  uint8_t tail[] = {0x00, 0x00, 0x80, 0x7f};
+  memcpy(buf + bytes, tail, 4);
+
+  bsp_uart_send(uart, (uint8_t *)buf, bytes + 4);
+}
+
+#endif // BSP_INCLUDE_BSP_VOFA_H_
